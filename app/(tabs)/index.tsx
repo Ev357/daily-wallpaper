2a68c122bfe8ac09d5b0a8bd61e7e77435cc90e7
Ev357 <evest.dev@gmail.com>
2024-07-21 23:38:54 +0200

    feat: basic test change wallpaper functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { setWallpaper, TYPE_SCREEN } from "rn-wallpapers";
 
 export default function HomeScreen() {
   return (
     <ThemedView style={styles.container}>
       <ThemedText>Hello World</ThemedText>
+      <Button onPress={set} title="Change Wallpaper" />
     </ThemedView>
+  );
+}
+
+async function set() {
+  await setWallpaper(
+    {
+      uri: "https://i0.wp.com/techwek.com/wp-content/uploads/2021/01/wallpaper-gotas-de-chuva.jpg",
+    },
+    TYPE_SCREEN.HOME
   );
 }
 
