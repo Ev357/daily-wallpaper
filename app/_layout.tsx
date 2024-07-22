161a566c5cc9828800dcf725612672b7ca3e98c5
Ev357 <evest.dev@gmail.com>
2024-07-22 18:36:03 +0200

    feat: activity indicator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,14 +4,17 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import { useColorScheme } from "react-native";
+import { ActivityIndicator, useColorScheme } from "react-native";
 import { darkTheme, lightTheme } from "@/constants/themes";
+import { UView } from "@/components/ui/View";
+import { useColors } from "@/hooks/useColors";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   const colorScheme = useColorScheme();
+  const colors = useColors();
   const [loaded] = useFonts({
     Geist: require("../assets/fonts/Geist-Regular.ttf"),
   });
@@ -23,7 +26,13 @@ const RootLayout = () => {
   }, [loaded]);
 
   if (!loaded) {
-    return null;
+    return (
+      <UView
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <ActivityIndicator size="large" color={colors.foreground} />
+      </UView>
+    );
   }
 
   return (
