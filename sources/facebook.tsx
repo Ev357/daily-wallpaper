19892f80df8690f6a281cb0c5e59a5b8dae37d7b
Ev357 <evest.dev@gmail.com>
2024-07-25 02:07:59 +0200

    feat: facebook onclick validation

--- sources/facebook.tsx
+++ sources/facebook.tsx
@@ -3,7 +3,10 @@ import SourceTiming from "@/components/SourceTiming";
 import { UButton } from "@/components/ui/button";
 import { USeparator } from "@/components/ui/separator";
 import { UText } from "@/components/ui/text";
-import { SourceContextProvider } from "@/contexts/sourceContext";
+import {
+  SourceContextProvider,
+  useSourceContext,
+} from "@/contexts/sourceContext";
 import { useColors } from "@/hooks/useColors";
 import { Source } from "@/sources/types";
 import { spacing } from "@expo/styleguide-base";
@@ -15,16 +18,38 @@ export const facebookSource: Source = {
 };
 
 export const Facebook = () => {
+  return (
+    <SourceContextProvider>
+      <FacebookScreen />
+    </SourceContextProvider>
+  );
+};
+
+export const FacebookScreen = () => {
   const colors = useColors();
+
+  const { source, dispatchSource } = useSourceContext();
+
+  const touchAll = () => {
+    dispatchSource({ type: "setTouchAll", touchAll: true });
+  };
 
   return (
     <View style={{ gap: spacing[2] }}>
-      <SourceContextProvider>
-        <SourceTiming />
-        <USeparator />
-        <SourceScreen />
-      </SourceContextProvider>
-      <UButton>
+      <SourceTiming />
+      <USeparator />
+      <SourceScreen />
+
+      <UButton
+        onPress={() => {
+          if (!source.isValid) {
+            touchAll();
+            return;
+          }
+
+          // TODO: Save
+        }}
+      >
         <UText style={{ color: colors.primaryForeground, textAlign: "center" }}>
           Save
         </UText>
